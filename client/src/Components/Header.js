475001f1fb17dca9f25c78f4dc95cb35b1c6e4a8
Alexander Varney <alex@varney.me>
2020-01-04 23:45:15 -0500

    Update header style

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -2,26 +2,30 @@ import React from "react";
 import { connect } from "react-redux";
 import { logout } from "../Store/Actions/auth";
 import { Link } from "react-router-dom";
-import styled from 'styled-components'
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  background: #f9f9f9;
+  color: #434343;
+  font-family: "Roboto Slab", "serif";
+  border-bottom: 1px solid rgba(0, 0, 0, 0.07);
+  user-select: none;
+  a,
+  a:visited {
+    color: #434343;
+  }
+  a:hover {
+    color: lightgrey;
+    text-decoration: none;
+  }
+`;
 
 function Header(props) {
-
-  const Nav = styled.nav`
-    background: dimgray;
-    color: #fff;
-    a, a:visited {
-      color: #fff;
-    }
-    a:hover {
-      color: lightgrey;
-      text-decoration: none;
-    }
-
-  `
-
   return (
     <Nav className="navbar">
-      <span className="navbar-brand mb-0 h1"><Link to="/">tinynote</Link></span>
+      <span className="navbar-brand mb-0 h1">
+        <Link to="/">tinynote</Link>
+      </span>
 
       {props.auth.loggedIn && props.auth.user ? (
         <div className="userInfo">
@@ -43,7 +47,9 @@ function Header(props) {
             Logout
           </a>
         </div>
-      ) : <Link to={'/user'}>Login</Link>}
+      ) : (
+        <Link to={"/user"}>Login</Link>
+      )}
     </Nav>
   );
 }
